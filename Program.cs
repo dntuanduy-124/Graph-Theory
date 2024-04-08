600d039e14ea78be8092edcf20d4bec021cb57b7
dntuanduy-124 <143875347+dntuanduy-124@users.noreply.github.com>
2024-04-08 07:22:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Lab10_2;
+
+Graph graph = new Graph();
+graph.CheckEuler01("EulerVoHuong.INP"); //Bai01
+graph.CheckEuler02("EulerCoHuong.INP"); //Bai02
+graph.EulerianCycle("ChuTrinhEuler.INP"); //Bai03
+graph.DrawAllEdges("kNet.INP"); //Bai04
