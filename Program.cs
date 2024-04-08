f743944def5b47e580b13bef98a07e3472c689fd
dntuanduy-124 <143875347+dntuanduy-124@users.noreply.github.com>
2024-04-08 07:15:24 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using _22DH110521_Lab05_DFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22DH110521_Lab05_DFS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+            //graph.ListCntVertices("LienThongDFS.INP");//Bai01
+            graph.PathDFS("TimDuongDFS.INP");//Bai02
+
+        }
+    }
+
+
+}
