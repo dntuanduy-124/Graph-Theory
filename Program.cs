942968328b831f443c0c3d8db0a0510e3f071131
dntuanduy-124 <143875347+dntuanduy-124@users.noreply.github.com>
2024-04-08 07:20:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab08ltdt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Graph graph = new Graph();
+            graph.GoMargin("RaBien.INP");
+            //graph.ChooseCity("ChonThanhPho.INP");
+            //graph.MoveCircle("DuongTron.INP");
+            //graph.Go2School("SCHOOL.INP");
+        }
+    }
+}
