2640c140299c592e26ccec7ebc821d21a581002e
Đỗ Ngọc Tuấn Duy <143875347+dntuanduy-124@users.noreply.github.com>
2024-10-28 22:51:16 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace _22DH110521_Lab2
             //graph.StorageTank("BonChua.INP");//Bai03
             //graph.TransposeGraph("ChuyenVi.INP");//Bai04
             //graph.AverageEdge("TrungBinhCanh.INP");//Bai05
-            graph.AdjacencyMatrix2AdjacencyList("MaTran2DSKe.INP");//BT 1 extra
+            graph.AdjacencyMatrix2AdjacencyList("MaTran2DSKe.INP");
             Console.ReadLine();
             
         }
